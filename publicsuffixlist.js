77b3edc75b453857d4018fdb99180f848a839daa
Raymond Hill <rhill@raymondhill.net>
2014-12-01 20:08:53 -0200

    Update publicsuffixlist.js

--- publicsuffixlist.js
+++ publicsuffixlist.js
@@ -4,22 +4,12 @@
     Mozilla Foundation's Public Suffix List <http://publicsuffix.org/list/>
     Copyright (C) 2013  Raymond Hill
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see {http://www.gnu.org/licenses/}.
-
+    License: pick the one which suits you:
+      GPL v3 see <https://www.gnu.org/licenses/gpl.html>
+      APL v2 see <http://www.apache.org/licenses/LICENSE-2.0>
 */
 
-/*! Home: https://github.com/gorhill/publicsuffixlist.js */
+/*! Home: https://github.com/gorhill/publicsuffixlist.js -- GPLv3 APLv2 */
 
 /*
     This code is mostly dumb: I consider this to be lower-level code, thus
